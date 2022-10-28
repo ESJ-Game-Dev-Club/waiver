0228f7db6636a857f10eb11197e972e8be558e6a
GlowInTheDark123 <steffes.james@gmail.com>
2022-10-28 12:46:14 -0400

    fixed mistake (thanks will) 	modified:   ui/hud.gd

--- ui/hud.gd
+++ ui/hud.gd
@@ -2,5 +2,5 @@ extends CanvasLayer
 
 
 func _process(delta):
-a	$Control/Health.text = "Health: " + str(GlobalVars.player.health)
+	$Control/Health.text = "Health: " + str(GlobalVars.player.health)
 	$Control/State.text = GlobalVars.player.States.keys()[GlobalVars.player.current_state]
