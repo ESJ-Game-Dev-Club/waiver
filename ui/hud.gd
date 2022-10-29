f6e6648aa106e5c121672624e06a33a9bc01b66a
GlowInTheDark123 <steffes.james@gmail.com>
2022-10-28 20:48:26 -0400

    game no longer crashes when you hit zero health 	modified:   project.godot 	modified:   ui/hud.gd

--- ui/hud.gd
+++ ui/hud.gd
@@ -2,5 +2,9 @@ extends CanvasLayer
 
 
 func _process(delta):
-	$Control/Health.text = "Health: " + str(GlobalVars.player.health)
+	if (GlobalVars.player.health):
+		$Control/Health.text = "Health: " + str(GlobalVars.player.health)
+	else:
+		$Control/Health.text = "Health: 0"
+		
 	$Control/State.text = GlobalVars.player.States.keys()[GlobalVars.player.current_state]
