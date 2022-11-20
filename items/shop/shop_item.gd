bda5291ec6a8288d7e09ff37c806a929a530f2ee
Chupun <77649437+Chupun@users.noreply.github.com>
2022-11-19 23:11:29 -0500

    fix shop

--- items/shop/shop_item.gd
+++ items/shop/shop_item.gd
@@ -3,7 +3,7 @@ extends Panel
 export var slot_no:int = 0
 var price = 0
 
-func display():
+func display(): #adds information to shop
 	price = owner.shop_prices[slot_no]
 	$Texture.texture = owner.items[slot_no].inventory_icon
 	$ItemLabel.text = str(owner.items[slot_no])
@@ -11,11 +11,11 @@ func display():
 
 
 
-func _on_ItemBuy_pressed():
+func _on_ItemBuy_pressed(): #handles purchacing from a shop
 	if Global.player.kill_count < price:
 		pass
 	else:
 		Global.player.kill_count = Global.player.kill_count - price
-		Global.player.pickup_item(owner.shop_items[slot_no].item_type, owner.shop_items[slot_no].inventory_icon, owner.shop_items[slot_no].item_scene_path)
+		Global.player.pickup_item(owner.items[slot_no].item_type, owner.items[slot_no].inventory_icon, owner.items[slot_no].item_scene_path)
 		$ItemBuy.disabled = true
 		$ItemBuy.text = "Sold Out"
