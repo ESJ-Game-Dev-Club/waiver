c0ff5e28c3673415cbeafb3ea6f2ddda50af7777
JimmyJohn1026 <jmillion627@gmail.com>
2022-11-20 12:08:50 +0000

    change enemy filepaths and modify hivemind a bit

--- enemy/hivemind.gd
+++ enemy/hivemind.gd
@@ -4,10 +4,10 @@ extends Node
 var rng = RandomNumberGenerator.new()
 var enemy_count = 0
 
-enum {
-	ENEMY_REG,
-	ENEMY_DASHER,
-	ENEMY_TANK,
+enum Enemy {
+	REG,
+	DASHER,
+	TANK,
 }
 
 
@@ -27,9 +27,9 @@ func _spawn_enemy(enemy_type: int):
 	var enemy = preload("res://enemy/enemy.tscn")
 	
 	match (enemy_type):
-		ENEMY_REG: enemy = preload("res://enemy/enemy.tscn")
-		ENEMY_DASHER: enemy = preload("res://enemy/dasher/dasher_enemy.tscn")
-		ENEMY_TANK: enemy = preload("res://enemy/tank/tank_enemy.tscn")
+		Enemy.REG: enemy = preload("res://enemy/enemy.tscn")
+		Enemy.DASHER: enemy = preload("res://enemy/dasher/enemy.tscn")
+		Enemy.TANK: enemy = preload("res://enemy/tank/enemy.tscn")
 
 	var e = enemy.instance()
 	e.position = _random_point() + Global.player.position # add player offset
@@ -40,7 +40,7 @@ func new_wave():
 	enemy_count += 5
 	
 	for i in range(enemy_count):
-		var type = rng.randi_range(1, 3)
+		var type = rng.randi_range(0, 2)
 		_spawn_enemy(type)
 
 
